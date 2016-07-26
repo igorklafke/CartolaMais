fc803ddd0dd273f78b118336499a4ad01bae2098
Tiago <tiago_almeida_rosa@hotmail.com>
2016-07-26 15:59:08 -0300

    Adicionando Tela Inicial e Tela de Login - COMMIT PARCIAL

--- CartolaMais/CartolaMais/App.cs
+++ CartolaMais/CartolaMais/App.cs
@@ -11,20 +11,54 @@ namespace CartolaMais
     {
         public App()
         {
+            //SE JÁ FOI FEITO LOGIN, REDIRECIONAR PARA A TELA INICIAL
+
             // The root page of your application
+            Image imageCartolaMais = new Image
+            {
+                Source = "cartolafcmaislargo.png",
+                WidthRequest = 120,
+                HeightRequest = 40
+            };
+            Label bemVindo = new Label
+            {
+                HorizontalTextAlignment = TextAlignment.Center,
+                Text = "Bem vindo ao Cartola Mais!"
+            };
+            Label entrar = new Label
+            {
+                HorizontalTextAlignment = TextAlignment.Center,
+                Text = "Para entrar, efetue seu login no Cartola!"
+            };
+            Button btnLogin = new Button
+            {
+                BackgroundColor = Color.FromHex("FF6A00"),
+                TextColor = Color.White,
+                Text = "Login",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            btnLogin.Clicked += OnLogin;
+
             MainPage = new ContentPage
             {
                 Content = new StackLayout
                 {
                     VerticalOptions = LayoutOptions.Center,
                     Children = {
-                        new Label {
-                            HorizontalTextAlignment = TextAlignment.Center,
-                            Text = "Bem vindo ao Cartola Mais!"
-                        }
+                        imageCartolaMais,
+                        bemVindo,
+                        entrar,
+                        btnLogin
                     }
                 }
             };
+        }
+
+        public void OnLogin(object sender, EventArgs e)
+        {
+            var pagina = new Views.Login();
+            MainPage = new NavigationPage(pagina);
         }
 
         protected override void OnStart()
