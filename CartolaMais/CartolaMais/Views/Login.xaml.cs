8054b6fa4d043fbd6d2ea18a6807654eed3f5a9c
Igor Klafke <igor.klafke@gmail.com>
2016-08-02 22:14:02 -0300

    Chamada web async

--- CartolaMais/CartolaMais/Views/Login.xaml.cs
+++ CartolaMais/CartolaMais/Views/Login.xaml.cs
@@ -15,14 +15,14 @@ namespace CartolaMais.Views
             InitializeComponent();
         }
 
-        public void OnEntrar(object sender, EventArgs e)
+		public async void OnEntrar(object sender, EventArgs e)
         {
 			//var pagina = new Views.Login();
 			//MainPage = new NavigationPage(pagina);
 			Cartola cartolaService = new Cartola();
-			string result = cartolaService.Autenticar(email.Text, senha.Text).Result;
+			string result = await cartolaService.Autenticar(email.Text, senha.Text);
 
-			DisplayAlert("Auth", result, "ok");
+			await DisplayAlert("Auth", result, "ok");
         }
     }
 }
